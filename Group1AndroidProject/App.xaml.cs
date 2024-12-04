d3e674559a152922640eb2bfbfdef8216d26c085
rafalh26 <rafalh26@gmail.com>
2024-12-04 06:01:42 +0100

    Add project files.

---
+++ Group1AndroidProject/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Group1AndroidProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
