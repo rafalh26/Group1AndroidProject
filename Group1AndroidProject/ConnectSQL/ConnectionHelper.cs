235a047089c353d8cf4b6aaa8c5bbe458866dc61
rafalh26 <rafalh26@gmail.com>
2024-12-08 12:09:00 +0100

    ...

--- Group1AndroidProject/ConnectSQL/ConnectionHelper.cs
+++ Group1AndroidProject/ConnectSQL/ConnectionHelper.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Android.App;
 using Java.Nio.FileNio.Attributes;
 using Npgsql;
+using Group1AndroidProject.ConnectSQL;
+using Security;
+using System.Data;
 
 namespace Group1AndroidProject.ConnectSQL
 {
@@ -14,6 +17,7 @@ namespace Group1AndroidProject.ConnectSQL
 
         public bool initialConnection = true;
         private const string ConnectionString = "User Id=postgres.jtjdvjrcxbdrdlgyqmzf;Password=testB@s3SQL123;Server=aws-0-eu-central-1.pooler.supabase.com;Port=6543;Database=postgres;";
+        Contact contact;
 
         public bool CheckConnection()
         {
@@ -31,8 +35,41 @@ namespace Group1AndroidProject.ConnectSQL
             }
             return true;
         }
-        private void sendCustomQuery(string customQuery)
+        private List<Contact> sendCustomQuery(string customQuery, Page currentPage)
         {
+            List<Contact> contacts = new List<Contact>();
+
+            using (NpgsqlConnection sqlConnection = new NpgsqlConnection(ConnectionString))
+            {
+                try
+                {
+                    sqlConnection.Open();
+
+                    using (NpgsqlCommand sqlCommand = new NpgsqlCommand(customQuery, sqlConnection))
+                    {
+                        using (NpgsqlDataReader reader = sqlCommand.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Contact contact = new Contact
+                                {
+                                    id = reader.GetInt32(reader.GetOrdinal("id")), // Adjust column name
+                                    name = reader.GetString(reader.GetOrdinal("name")), // Adjust column name
+                                    email = reader.GetString(reader.GetOrdinal("email")), // Adjust column name
+                                    phone = reader.GetOrdinal("phone")
+                                };
+                                contacts.Add(contact);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    currentPage.DisplayAlert("Warning", ex.Message, "Ok");
+                }
+            }
+
+            return contacts;
         }
     }
 }
---
+++ Group1AndroidProject/ConnectSQL/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group1AndroidProject.ConnectSQL
+{
+    internal class Contact
+    {
+        public int id { get; set; }
+        public string nick { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public int? phone { get; set; }
+        public string? address { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
