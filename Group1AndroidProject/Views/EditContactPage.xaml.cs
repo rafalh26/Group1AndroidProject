da3114983b6db98c5c587e117fda7e5e9b83fc26
rafalh26 <rafalh26@gmail.com>
2024-12-11 06:36:01 +0100

    ...

--- Group1AndroidProject/Views/EditContactPage.xaml.cs
+++ Group1AndroidProject/Views/EditContactPage.xaml.cs
@@ -1,9 +1,35 @@
+using Group1AndroidProject.Models;
+using Microsoft.Maui.Platform;
+using System.Reflection.Emit;
+
 namespace Group1AndroidProject.Views;
 
 public partial class EditContactPage : ContentPage
 {
-	public EditContactPage()
+    public ConnectionHelper connectionHelper { get; set; }
+
+    public EditContactPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+
+    private void saveButton_Clicked(object sender, EventArgs e)
+    {
+        connectionHelper = new ConnectionHelper();
+        if (string.IsNullOrEmpty(nameEntry) || string.IsNullOrEmpty(emailEntry))
+        {
+            DisplayAlert("Required fields error!", "Hey dude are U sure that You are nameless?\n Did You also forgot your internet personality existance by not sharing Your email address with us?\n Play nicely and provide required data");
+        }
+        else
+        {
+            connectionHelper
+        }
+
+
+
+
+
+
+
+    }
 }
